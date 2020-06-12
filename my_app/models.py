183cd8da9bf4c09f1fba5e1018ca15cae6118ab1
ro-SH <shadko.roma@mail.ru>
2020-06-12 20:34:25 +0300

    Added Task visualization

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=200)
+    deadline = models.DateTimeField('Time')
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    is_done = models.BooleanField('Completed')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Tasks'
+        ordering = ['is_done', 'deadline']
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Categories, Tasks
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'my_app/index.html')
+    categories = Categories.objects.all()
+    context = {
+        'category_list': categories,
+    }
+    return render(request, 'my_app/index.html', context)

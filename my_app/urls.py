8abda41ca20391df7b73f9fcb172230a41c2d5fa
ro-SH <shadko.roma@mail.ru>
2020-06-13 15:09:46 +0300

    Added Add function

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'my_app'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('delete_todo/<int:task_id>', views.delete_todo, name='delete_todo'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import HttpResponseRedirect, render
 from .models import Categories, Tasks
 
-# Create your views here.
+DATETIME_FORMAT = '%b %d, %Y %I:%M %p'
 
 def home(request):
     categories = Categories.objects.all()
@@ -9,3 +10,31 @@ def home(request):
         'category_list': categories,
     }
     return render(request, 'my_app/index.html', context)
+
+
+def add_todo(request):
+    categories = Categories.objects.all()
+    context = {
+        'category_list': categories,
+    }
+    try:
+        category = Categories.objects.get(pk=request.POST.get('category'))
+        to_do = request.POST.get('to_do')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        formatted_date = datetime.strptime(date + ' ' + time, DATETIME_FORMAT)
+        is_done = False
+    except (KeyError, Categories.DoesNotExist):
+        context['error_message'] = "You did not select a category."
+        return render(request, 'my_app/index.html', context)
+    except ValueError:
+        context['error_message'] = "Incorrect date or time."
+        return render(request, 'my_app/index.html', context)
+    
+    Tasks.objects.create(title=to_do, deadline=formatted_date, category=category, is_done=False)
+    return HttpResponseRedirect('/')
+
+
+def delete_todo(request, task_id):
+    print(task_id)
+    return HttpResponseRedirect('/')
